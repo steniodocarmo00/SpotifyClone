f46cb2e508c4db664bc672606547cc70b173f50b
Stenio do Carmo <steniobotelhodocarmo@gmail.com>
2023-04-16 23:18:51 -0300

    feat(Signin): new login screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
 import { Text, View, StatusBar } from 'react-native';
-
+import { NativeBaseProvider } from 'native-base';
 import { useFonts, Inter_400Regular, Inter_800ExtraBold } from '@expo-google-fonts/inter'
+
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_800ExtraBold});
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#1E1E1E'}} >
+    <NativeBaseProvider>
       <StatusBar 
         barStyle="light-content" 
         backgroundColor='transparent'
-        translucent
+        translucent 
       />
-      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
-    </View>
+      {fontsLoaded ? <SignIn /> : <View />}
+    </NativeBaseProvider>
   );
 }
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { VStack, Center, Heading } from 'native-base'
+
+export function SignIn(){
+  return(
+    <VStack flex={1} bg='#1E1E1E' alignItems='center' justifyContent='center'>
+      <Heading>
+        Hello World
+      </Heading>
+    </VStack>
+  )
+}
