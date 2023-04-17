03a00cea2c120f79cf037d47a948bda908ad2170
Stenio do Carmo <steniobotelhodocarmo@gmail.com>
2023-04-17 04:11:38 -0300

    style(Signin): improves input props

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -6,8 +6,13 @@ export function SignIn(){
   return(
     <VStack flex={1} bg='#1E1E1E' alignItems='center' justifyContent='center'>
       <Center>
-        <Input placeholder='LOGIN'/>
-        <Input placeholder='SENHA'/>
+        <Input 
+          placeholder='Login'
+        />
+        <Input 
+          placeholder='Senha'
+          secureTextEntry
+        />
       </Center>
     </VStack>
   )
