af7bac7c5ce67744ff3bad37053a479f31a7e492
Stenio do Carmo <steniobotelhodocarmo@gmail.com>
2023-04-16 22:01:57 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
+
+import { useFonts, Inter_400Regular, Inter_800ExtraBold } from '@expo-google-fonts/inter'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_800ExtraBold});
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#1E1E1E'}} >
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
