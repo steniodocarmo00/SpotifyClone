4307f261b29412bae9f2701a4559862748070ded
Stênio do Carmo <steniobotelhodocarmo@gmail.com>
2023-04-19 08:51:00 -0300

    feat(ArtistName) created artist name component

---
+++ src/components/ArtistName.tsx
@@ -0,0 +1,26 @@
+import { VStack, HStack, Text, Image } from "native-base";
+
+import PlaylistIcon from '@assets/playlistIcon.png';
+
+export function ArtistName(){
+  return(
+  <HStack alignItems='center'>
+    <Image 
+      source={PlaylistIcon}
+      alt="Icone do artista"
+      w={4}
+      h={4}
+      rounded={10}
+      ml={4}
+      mr={2}
+    />
+        
+    <VStack alignItems='center'>
+      <Text color='white' fontSize='xs' fontFamily='bodybold'>
+        Fábio VIREI CANIBAL
+      </Text>
+
+    </VStack>
+  </HStack>
+  )
+}
